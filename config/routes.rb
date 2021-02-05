b5b6b076057143d47287b813a771fa4cd41a7156
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-05 15:57:30 +0500

    Give links to main and single pages to each other

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: "movie"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
