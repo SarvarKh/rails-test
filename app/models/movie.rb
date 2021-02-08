1bdeaec12184671bc1302b84554b49530fd26734
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-08 19:28:49 +0500

    Connect Review with Movie model via associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+    has_many :reviews, dependent: :destroy
+
     validates :name, :description, :location, presence: true
 
     validates :description, length: {minimum: 25}
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,19 @@
 class Review < ApplicationRecord
   belongs_to :movie
+
+  validates :name, presence: true
+
+  validates :email, format: {with: /(\S+)@(\S+)/ }
+
+  HOW_HEARD_OPTIONS = [
+    'News letter',
+    'Blog Post',
+    'Twitter',
+    'Web Search',
+    'Friend/Coworker',
+    'Other'
+  ]
+
+  validates :how_heard,
+            inclusion: { in: HOW_HEARD_OPTIONS }
 end
